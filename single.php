5e42afb49a191590189d8fbbbab6b524f823a6ca
bikohara <160682339+bikohara@users.noreply.github.com>
2024-11-14 17:43:15 +0900

    Update single.php

--- single.php
+++ single.php
@@ -7,21 +7,36 @@
 	</div>
 </div>
 <?php output_breadcrumb(); ?>
-<div id="main">
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<div id="post-<?php the_ID(); ?>" class="content">
-		<h3 class="heading"><?php the_title(); ?></h3>
-		<div class="post">
-			<p class="dateLabel"><time datetime="<?php the_time('Y-m-d')?>"><?php the_time('Y/m/d')?></time>
+<div id="wrapper">
+	<div id="content">
+		<div id="side-contents">
+			<div id="post-<?php the_ID(); ?>" class="main-data">
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<h3 class="heading"><?php the_title(); ?></h3>
+<?php if(has_post_thumbnail()): ?>
+				<figure class="post-images">
+					<img src="<?php echo the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+				</figure>
+<?php endif; ?>
 <?php the_content(); ?>
+				<p class="post-day"><time datetime="<?php the_modified_time('c'); ?>"><?php the_time('Y.m.j'); ?></time></p>
+<?php
+if (comments_open() || get_comments_number()){
+	comments_template();
+}
+endwhile;
+?>
+			</div>
+			<div class="side-data">
+<?php get_sidebar(); ?>
+			</div>
 		</div>
-	</div>
-	<?php endwhile;?>
 	<div class="pagenav">
 		<div class="prev_post"><?php next_post_link('%link', __('Next Post', 'sample')); ?></div>
 		<div class="next_post"><?php previous_post_link('%link', __('Previous Post', 'sample')); ?></div>
 	</div>
 <?php endif; ?>
+	</div>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
