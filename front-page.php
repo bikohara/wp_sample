4ea919971c154c9abcbdef63e4825eaaba3e44d5
bikohara <160682339+bikohara@users.noreply.github.com>
2025-04-01 17:11:28 +0900

    Delete front-page.php

--- front-page.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<?php $header_image = get_header_image();
-if ($header_image): ?>
-<figure id="mainImg">
-	<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="<?php bloginfo('description'); ?>">
-</figure>
-<?php endif;?>
-<?php get_template_part('template/top-page'); ?>
-<?php get_footer(); ?>
