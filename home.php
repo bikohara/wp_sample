2124179bb4a9d3b23c2ff7a5be5e8224f2ceda0a
bikohara <160682339+bikohara@users.noreply.github.com>
2025-04-16 12:10:44 +0900

    Update home.php

--- home.php
+++ home.php
@@ -9,25 +9,29 @@
 <?php output_breadcrumb(); ?>
 <div id="content">
 	<div class="inner">
-<?php $args = array(
+<?php
+$args = array(
 	'post_type' => 'post',
 	'posts_per_page' => 8,
 	'paged' => $paged
-); ?>
-<?php $the_query = new WP_Query($args); ?>
-<?php if(have_posts()) : ?>
-<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+);
+$the_query = new WP_Query($args); ?>
+if(have_posts()) :
+	while ($the_query->have_posts()) : $the_query->the_post();
+?>
 			<dl class="news">
-				<dt><?php the_time('Y/m/d');?><?php
+				<dt><?php the_time('Y/m/d'); ?><?php
 $category = get_the_category();
 if ($category[0]) {
 	echo '<a class="cate" href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->cat_name . '</a>';
 } ?></dt>
 				<dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
 			</dl>
-<?php endwhile;
+<?php
+	endwhile;
 endif;
-the_posts_pagination(array('screen_reader_text'=>'')); ?>
+the_posts_pagination(array('screen_reader_text'=>''));
+?>
 	</div>
 </div>
 <?php get_footer(); ?>
