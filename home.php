22b282dd56247555ae8bfa088123a62753f3e4fd
bikohara <160682339+bikohara@users.noreply.github.com>
2025-10-16 18:34:12 +0900

    Update home.php

--- home.php
+++ home.php
@@ -15,22 +15,25 @@ $args = array(
 	'posts_per_page' => 8,
 	'paged' => $paged
 );
-$the_query = new WP_Query($args); ?>
-if(have_posts()) :
+$the_query = new WP_Query($args);
+if ($the_query->have_posts()) :
 	while ($the_query->have_posts()) : $the_query->the_post();
 ?>
-			<dl class="news">
-				<dt><?php the_time('Y/m/d'); ?><?php
-$category = get_the_category();
-if ($category[0]) {
-	echo '<a class="cate" href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->cat_name . '</a>';
-} ?></dt>
-				<dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
-			</dl>
+	<dl class="news">
+		<dt><?php the_time('Y/m/d'); ?>
+		<?php
+		$category = get_the_category();
+		if ($category[0]) {
+			echo '<a class="cate" href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->cat_name . '</a>';
+		}
+		?>
+		</dt>
+		<dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
+	</dl>
 <?php
 	endwhile;
 endif;
-the_posts_pagination(array('screen_reader_text'=>''));
+the_posts_pagination(array('screen_reader_text' => ''));
 ?>
 	</div>
 </div>
